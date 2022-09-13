1c0f67c1105396b79cac405eb280307d64d9a71b
umezeyi-equisys <113022563+umezeyi-equisys@users.noreply.github.com>
2022-09-13 08:13:40 +0100

    4 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, VERSION } from '@angular/core';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
